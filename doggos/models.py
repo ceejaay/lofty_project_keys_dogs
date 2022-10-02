374004ff4c7abebd21992cbeceec86d7a019f12a
Chad Jemmett <cjemmett@wehco.com>
2022-10-02 15:58:04 -0500

    Can save a dog to the database. Wooo

--- doggos/models.py
+++ doggos/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class DogImage(models.Model):
     img = models.ImageField(upload_to='images')
 
+
+class DuplicateDog(models.Model):
+    img = models.ImageField(upload_to="images")
+    original_dog_id = models.ForeignKey(DogImage, on_delete=models.CASCADE)
 # Create your models here.
--- doggos/views.py
+++ doggos/views.py
@@ -4,13 +4,33 @@ from doggos.models import DogImage
 from rest_framework import viewsets
 from rest_framework import permissions
 from doggos.serializers import DogSerializer
+import json
+import requests
+import tempfile
+from django.core import files
+RANDOM = 'https://dog.ceo/api/breeds/image/random'
 
 class DogViewSet(viewsets.ModelViewSet):
     queryset = DogImage.objects.all()
     serializer_class = DogSerializer
 
     def get_dog(self, request):
-        return HttpResponse("hello doggy")
+        response = requests.get(RANDOM).json()
+        dog_url = response['message']
+
+        i = requests.get(dog_url, stream=True)
+        if i.status_code == 200:
+            temp_file = tempfile.NamedTemporaryFile()
+        for block in i.iter_content(1024 * 8):
+            if not block:
+                break
+            temp_file.write(block)
+
+        dog_img = DogImage()
+        dog_img.img.save("new dog", files.File(temp_file))
+
+
+        return HttpResponse()
 
 
 # Create your views here
