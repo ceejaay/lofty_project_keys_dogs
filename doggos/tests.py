ab58cb60e60896c7ed2b46923300a930361045d2
Chad Jemmett <cjemmett@wehco.com>
2022-10-06 20:52:00 -0500

    Started some tests

--- doggos/tests.py
+++ doggos/tests.py
@@ -1,26 +1,39 @@
 from django.test import TestCase
 from django.core.files import File
 from datetime import timedelta, date
+import random
 import sys
+import pathlib
+import os
 from doggos.models import DogImage
-DOG_PIC = "images/test_dog.jpg"
+DOG_PIC = "images/test_doggy.jpg"
 # Create your tests here.
 
 class DogImageTest(TestCase):
 
     def setUp(self):
-       self. new_pic = DogImage()
+       self.new_pic = DogImage()
 
 
     def test_for_saved_photo(self):
-        self.new_pic.img.save("test_doggy.jpg", File(open(DOG_PIC, 'rb')))
+        new_dog_pic = f"saved_doggy{random.randint(0, 1000)}"
+        self.new_pic.img.save(new_dog_pic, File(open(DOG_PIC, 'rb')))
         dog_from_db = DogImage.objects.last()
         self.new_pic.height = 100
-        self.new_pic.width = 200
-        self.assertEqual('images/test_doggy.jpg', dog_from_db.img)
+        self.new_pic.width = 100
+        self.assertEqual("images/" + new_dog_pic, dog_from_db.img)
+        if os.path.isfile("images/" + new_dog_pic):
+            os.remove("images/" + new_dog_pic)
+        self.new_pic.save()
+        self.assertEqual(dog_from_db.height, 100)
+        self.assertEqual(dog_from_db.width, 100)
+
+
+
 
     def test_for_altered_photo(self):
-        pass
+        altered_pic = DogImage()
+
 
     def test_for_uploaded_photo(self):
         pass
